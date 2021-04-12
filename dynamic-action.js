8d9996a09c975dc4388c4d057b2b9221d5c54494
malekara <60537658+malekara@users.noreply.github.com>
2021-04-12 16:22:32 +0430

    create dynamic-action.js
    fetch the current records in Interactive grid

---
+++ dynamic-action.js
@@ -0,0 +1,26 @@
+/* + Event: 'Page change (Interactive Grid)'
+   + Selection type: 'Region' 
+   + Region: 'The Interactive Grid you add to the page' */
+
+
+
+
+
+var l_empno;
+// fetch the model for the interactive grid
+var model = apex.region("my_ig").widget().interactiveGrid("getViews", "grid").model;
+// loop through the records
+model.forEach(function(igrow) {
+   if(l_empno == null){
+       // first row without seperator ":"
+       l_empno = igrow[model.getFieldKey("DOC_SEQ")];
+   }
+   else {
+       // add seperator ":" to each additional row
+       l_empno = l_empno + ':' + igrow[model.getFieldKey("DOC_SEQ")];
+   }
+}
+);
+
+// Set Page Item
+apex.item( "P176_EMPNO_JS" ).setValue (l_empno);
